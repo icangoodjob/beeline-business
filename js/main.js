9ecaa95b4a6e7b0d7a85d88a255789ab6b83cdcd
Alexey <lxmlshv97@mail.ru>
2022-03-21 16:59:26 +0300

    add pages record-talk & scripts-2

--- js/main.js
+++ js/main.js
@@ -5,6 +5,10 @@ $(document).ready(function () {
 		$('#sidebar').toggleClass('active');
 		$('#wrapper').toggleClass('active');
 		$('#header').toggleClass('active');
+		if (!$('#sidebar').hasClass('active')) {
+			$('.aside .menu-dropdown').removeClass('show');
+			$('.aside .menu__link').addClass('collapsed');
+		}
 	});
 });
 
@@ -30,22 +34,49 @@ if (btnMore != null) {
 }
 
 // Календари
+// Сделаем неактивными воскресенье и субботу
+var disabledDays = [0, 6];
+
 $('#datepickerOne').each(function() {
 	$(this).datepicker({
+		navTitles: {
+			days: 'MM yyyy'
+		},
 		format: "dd/mm/yyyy",
 		weekends: [6,0],
 		inline: false,
 		autoclose: true,
 		position: 'bottom left',
+		onRenderCell: function (date, cellType) {
+			if (cellType == 'day') {
+				var day = date.getDay(),
+				isDisabled = disabledDays.indexOf(day) != -1;
+				return {
+					disabled: isDisabled
+				}
+			}
+		},
 	});
 });
 $('#datepickerTwo').each(function() {
 	$(this).datepicker({
+		navTitles: {
+			days: 'MM yyyy'
+		},
 		format: "dd/mm/yyyy",
 		weekends: [6,0],
 		inline: false,
 		autoclose: true,
 		position: 'bottom left',
+		onRenderCell: function (date, cellType) {
+			if (cellType == 'day') {
+				var day = date.getDay(),
+				isDisabled = disabledDays.indexOf(day) != -1;
+				return {
+					disabled: isDisabled
+				}
+			}
+		},
 	});
 });
 $('.datepicker--cells').click(function(){
@@ -61,16 +92,76 @@ for (let i = 0; i < toggleChoice.length; i++) {
 }
 
 /*================== Dropdown menu settings ====================*/
-$('#settings-btn').on('click', function (e){
-	$('.dropmenu').toggleClass('show');
-});
+let boxSettings = document.querySelector('.settings-box');
+document.addEventListener('click', dropMenuOpen);
+function dropMenuOpen(event) {
+	if (boxSettings) {
+		if (event.target.closest('.settings-btn')){
+			boxSettings.classList.toggle('show');
+			return false;
+		}
+		if (!event.target.closest('.settings-box')) {
+			boxSettings.classList.remove('show');
+		}
+	}
+}
+/*================== Dropdown menu settings ====================*/
+
+/*================== all btn ====================*/
+$('.all-btn').on('click', function(event) {
+	event.preventDefault();
+	$('.choice-search').removeClass('show');
+	$(this).parent('.choice-search').toggleClass('show');
+})
 $(document).mouseup(function (e){
-	var dropmenuContainer = $('.dropmenu'); 
-	if (!dropmenuContainer.is(e.target) && dropmenuContainer.has(e.target).length === 0) {
-		dropmenuContainer.removeClass('show'); 
+	let searchContainer = $('.choice-search'); 
+	if (!searchContainer.is(e.target) && searchContainer.has(e.target).length === 0) {
+		searchContainer.removeClass('show'); 
 	}
 });
-/*================== Dropdown menu settings ====================*/
+/*================== all btn ====================*/
+
+
+/*================== checkbox ====================*/
+$('#select-all').on('click', function(){
+	$(this).toggleClass('checked');
+	if ($(this).hasClass('checked')) {
+		$('.table-checkbox input:checkbox').prop('checked', true);
+	} else {
+		$('.table-checkbox input:checkbox').prop('checked', false);
+	}
+	return false;
+});
+/*================== checkbox ====================*/
+
+
+/*================== action button (show / hide) ====================*/
+// $('.table-checkbox input:checkbox').click(function(){
+// 	if ($('.table-checkbox input:checkbox') || $('#select-all')) {
+// 		$('.actions__button').removeAttr('disabled');
+// 		$('.actions__counter span').html($('.table-checkbox input:checkbox:checked').length);
+// 	}
+// });
+/*================== action button (show / hide) ====================*/
+
+
+
+/*================== redact file name ====================*/
+$('.file-name__text').on('click', function(event) {
+	$('.file-name').removeClass('show');
+	$(this).parents('.file-name').toggleClass('show');
+	$('.file-name__box').removeClass('hide');
+	$(this).parents('.file-name').find('.file-name__box').addClass('hide');
+})
+
+$(document).mouseup(function (e){
+	let fileContainer = $('.file-name');
+	if (!fileContainer.is(e.target) && fileContainer.has(e.target).length === 0) {
+		fileContainer.removeClass('show');
+		$('.file-name__text').parents('.file-name').find('.file-name__box').removeClass('hide');
+	}
+});
+/*================== redact file name ====================*/
 
 
 /*================== Offcanvas load files====================*/
@@ -85,7 +176,15 @@ if (filesItem) {
 }
 /*================== Offcanvas load files====================*/
 
+/*================== Offcanvas overlay====================*/
 
+$('#add-btn').on('click', function(){
+	$('.overlay-offcanvas').toggleClass('show');
+});
+$('[data-modal-close]').on('click', function(){
+	$('.overlay-offcanvas').toggleClass('show');
+});
+/*================== Offcanvas overlay====================*/
 
 /*================== RANGE-SLIDER ====================*/
 let rangeSliderTonality = document.getElementById('range-tonality');
@@ -121,3 +220,72 @@ if (rangeSliderSpeed){
 		}
 	});
 }
+
+// Графики
+const ctx = document.querySelectorAll('.chartBar');
+for (let chartItem of ctx) {
+	const myChart = new Chart(chartItem, {
+		type:'bar',
+		data: {
+			labels: [`Одновр. речь`, 'Одновр. тишина', 'Макс. длит. речи оператора'],
+			datasets: [{
+				label: 'First dataset',
+				data: [1, 4.7, 4],
+				backgroundColor: [
+				'#A0D911',
+				'#FFA940',
+				'#69C0FF',
+				],
+				borderColor: [
+				'#A0D911',
+				'#FFA940',
+				'#69C0FF',
+				],
+				borderWidth: 1,
+				borderRadius: 4,
+			}],
+		},
+		options: {
+			animation: true,
+			indexAxis: 'y',
+			tooltips: {
+				enabled: false
+			},
+			plugins: {
+				legend: {
+					display: false,
+					title: {
+						display: false,
+						text: '',
+					}
+				},
+				datalabels: {
+					color: '#000',
+					textAlign: 'center',
+					font: {
+						lineHeight: 1.6,
+					},
+					formatter: function(value) {
+						return value + '%';
+					},
+				}
+			},
+			scales: {
+				y: {
+					beginAtZero: true
+				}
+			// yAxes: [{
+			// 	ticks: {
+			// 		beginAtZero: true,
+			// 		fontFamily: "'Beeline Sans Bold', sans-serif",
+			// 		fontSize: 8,
+			// 	}
+			// }]
+		},
+		responsive: true, 
+		maintainAspectRatio: false,
+	},
+});
+}
+
+
